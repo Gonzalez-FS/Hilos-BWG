b39d26d7b90a7376ef0ba59d2e38ebda1b270b9d
Gonzalez-FS <108537743+Gonzalez-FS@users.noreply.github.com>
2022-08-28 21:53:18 -0300

    Add files via upload

---
+++ Hilos/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TXTBOX = New System.Windows.Forms.RichTextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Btn_Iniciar = New System.Windows.Forms.Button()
+        Me.Btn_Cancelar = New System.Windows.Forms.Button()
+        Me.Txt_valor = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TXTBOX
+        '
+        Me.TXTBOX.Location = New System.Drawing.Point(12, 107)
+        Me.TXTBOX.Name = "TXTBOX"
+        Me.TXTBOX.Size = New System.Drawing.Size(562, 231)
+        Me.TXTBOX.TabIndex = 0
+        Me.TXTBOX.Text = ""
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 12)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(562, 24)
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'Btn_Iniciar
+        '
+        Me.Btn_Iniciar.Location = New System.Drawing.Point(13, 63)
+        Me.Btn_Iniciar.Name = "Btn_Iniciar"
+        Me.Btn_Iniciar.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_Iniciar.TabIndex = 2
+        Me.Btn_Iniciar.Text = "Iniciar"
+        Me.Btn_Iniciar.UseVisualStyleBackColor = True
+        '
+        'Btn_Cancelar
+        '
+        Me.Btn_Cancelar.Location = New System.Drawing.Point(488, 63)
+        Me.Btn_Cancelar.Name = "Btn_Cancelar"
+        Me.Btn_Cancelar.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_Cancelar.TabIndex = 3
+        Me.Btn_Cancelar.Text = "Cancelar"
+        Me.Btn_Cancelar.UseVisualStyleBackColor = True
+        '
+        'Txt_valor
+        '
+        Me.Txt_valor.Location = New System.Drawing.Point(235, 66)
+        Me.Txt_valor.Name = "Txt_valor"
+        Me.Txt_valor.Size = New System.Drawing.Size(100, 20)
+        Me.Txt_valor.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(264, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Valor"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(587, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 18)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "0%"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(628, 350)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Txt_valor)
+        Me.Controls.Add(Me.Btn_Cancelar)
+        Me.Controls.Add(Me.Btn_Iniciar)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TXTBOX)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TXTBOX As RichTextBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Btn_Iniciar As Button
+    Friend WithEvents Btn_Cancelar As Button
+    Friend WithEvents Txt_valor As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Hilos/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Threading
+Imports System.IO.StringWriter
+Imports System.IO.StreamWriter
+Imports System.ComponentModel
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+    Private WithEvents BackGroundWorker1 As New BackgroundWorker
+    Private Sub Btn_Iniciar_Click(sender As Object, e As EventArgs) Handles Btn_Iniciar.Click
+        Btn_Iniciar.Enabled = False
+
+        BackGroundWorker1.WorkerReportsProgress = True
+        BackGroundWorker1.WorkerSupportsCancellation = True
+        BackGroundWorker1.RunWorkerAsync()
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackGroundWorker1.DoWork
+        For I = 1 To CInt(Txt_valor.Text)
+            My.Computer.FileSystem.WriteAllText(Application.StartupPath & "\Lineas.TXT", "Línea N° " & I & vbCrLf, True)
+            If BackGroundWorker1.CancellationPending Then
+                Exit Sub
+            End If
+            BackGroundWorker1.ReportProgress(CInt(100 * I / CInt(Txt_valor.Text)))
+        Next
+    End Sub
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles BackGroundWorker1.ProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+        Label2.Text = ProgressBar1.Value & " %"
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackGroundWorker1.RunWorkerCompleted
+        BackGroundWorker1.Dispose()
+    End Sub
+
+    Private Sub Btn_Cancelar_Click(sender As Object, e As EventArgs) Handles Btn_Cancelar.Click
+        BackGroundWorker1.CancelAsync()
+        Btn_Iniciar.Enabled = True
+        Label2.Text = "0%"
+        TXTBOX.Text = ""
+        ProgressBar1.Value = 0
+    End Sub
+End Class
